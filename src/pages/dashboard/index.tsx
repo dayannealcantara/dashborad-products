4a2a8a91dcb4f10514b1bee8b8bd1556406e96b5
Dayanne Alcantara <dayannealcantara14@gmail.com>
2024-06-25 16:42:36 -0300

    feat: removendo chamada de api e passando por componente service

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,45 +1,44 @@
 import Card from "../../components/Card";
 import OrderList, { Order } from "../../components/List";
-import { AppContainer, Grid, Title } from "./style";
-import axios from 'axios';
+import { api } from "../../service/api";
+import { Container, AppContainer, Grid, Title } from "./style";
+
 import { useEffect, useState } from "react";
-
-
-const api = axios.create({
-  baseURL: 'http://localhost:3000',
-});
-
 const Dashboard = () => {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
-  async function listProduct () {
-    try{
-      const response = await api.get('/products')
-      setProducts(response.data)
-
-    }catch(err){
-      console.log(err)
+  async function listProduct() {
+    try {
+      const response = await api.get("/products");
+      setProducts(response.data);
+    } catch (err) {
+      console.log(err);
     }
   }
 
-  useEffect(()=>{
-    listProduct()
-  },[])
+  useEffect(() => {
+    listProduct();
+  }, []);
 
-  function totalValue (products:Order[]) {
-    return products.reduce((acc, order) => acc + order.valorTotal, 0); 
+  function totalValue(products: Order[]) {
+    return products.reduce((acc, order) => acc + order.valorTotal, 0);
   }
- 
-  return(
-    <AppContainer>
-      <Title>Pedidos</Title>
-      <Grid>
-        <Card title="Total de Pedidos" value={`${products.length} unidade`} />
-        <Card title="Valor total" value={`R$ ${totalValue(products).toFixed(2)}`} />    
-      </Grid>
-      <OrderList orders={products} /> 
-    </AppContainer>
-  )
-}
 
-export default Dashboard
+  return (
+    <Container>
+      <AppContainer>
+        <Title>Pedidos</Title>
+        <Grid>
+          <Card title="Total de Pedidos" value={`${products.length} unidade`} />
+          <Card
+            title="Valor total"
+            value={`R$ ${totalValue(products).toFixed(2)}`}
+          />
+        </Grid>
+        <OrderList orders={products} />
+      </AppContainer>
+    </Container>
+  );
+};
+
+export default Dashboard;
